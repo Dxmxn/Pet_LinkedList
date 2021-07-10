cca675250c08e6c34523c45d5aafbed0895ba222
Faizullin Daniil <qwertin30@mail.ru>
2021-07-10 12:31:11 +0400

    class Item completed

--- Pet_LinkedList/Model/Item.cs
+++ Pet_LinkedList/Model/Item.cs
@@ -6,10 +6,30 @@ using System.Threading.Tasks;
 
 namespace Pet_LinkedList.Model
 {
-    class Item<T>
+    public class Item<T>
     {
-        private T Data = default(T);
-        private Item<T> next = null;
+        private T data = default(T); 
+        public T Data 
+        {
+            get { return data; }
+            set 
+            {
+                if (value != null)
+                    data = value;
+                else
+                    throw new ArgumentNullException(nameof(value));
+            }
+        }
 
+        public Item<T> Next { get; set; }
+        public Item(T Data)
+        {
+            Data = data;
+        }
+
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
     }
 }
