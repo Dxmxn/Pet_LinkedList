7d4d611ba554c3f522fef6d6a8d72d71e01949cd
UnName <UnName@DESKTOP-434GJ0H>
2021-07-10 12:19:05 +0400

    Добавьте файлы проекта.

---
+++ Pet_LinkedList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pet_LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
